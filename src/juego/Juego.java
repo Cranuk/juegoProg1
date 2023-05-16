8c469f0ab2563038289320c8041f9955827c7814
FlorSangueso <sanguesof@gmail.com>
2023-05-15 23:47:46 -0300

    toque juego, destructores pero falta incluir a todos los destrcutores, y proyectil

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -1,6 +1,5 @@
 package juego;
 
-
 import java.awt.Color;
 import java.awt.Font;
 import java.util.Random;
@@ -13,8 +12,9 @@ public class Juego extends InterfaceJuego
 	// El objeto Entorno que controla el tiempo y otros
 	private Entorno entorno;
 	private Astromega miAstromega;
-	private Destructor miDestructores;
+	private Destructor[] destructores;
 	private boolean perdido;
+	private Proyectil miProyectil;
 	private Asteroide miAsteroide;
 	
 	// Variables y métodos propios de cada grupo
@@ -30,8 +30,15 @@ public class Juego extends InterfaceJuego
 		// ...
 		this.miAstromega= new Astromega(400, 550, 150, 40,2);
 		
-		this.miDestructores= new Destructor(rand.nextInt(750),200,130,30,2);
-		Asteroide Asteroides[] = new Asteroide[5];
+		this.destructores=new Destructor[4];
+		this.destructores[0]= new Destructor(rand.nextInt(800),130,100,30,2);
+		this.destructores[1]= new Destructor(rand.nextInt(710),60,100,30,2);
+		this.destructores[2]= new Destructor(rand.nextInt(730),160,100,35,2);
+		this.destructores[3]= new Destructor(rand.nextInt(600),140,100,45,2);
+		
+		this.miProyectil= new Proyectil(450,300,15,50,2);
+		this.miAsteroide=new Asteroide(rand.nextInt(650),100,50,1);
+		
 
 		// Inicia el juego!
 		this.entorno.iniciar();
@@ -50,15 +57,32 @@ public class Juego extends InterfaceJuego
 		// ...
 		if (!perdido) {
 			this.miAstromega.dibujarse(this.entorno);
-			this.miDestructores.dibujarse(this.entorno);
-			this.miDestructores.mover();
 			this.miAsteroide.dibujarse(this.entorno);
+			this.miProyectil.dibujarse(this.entorno);
+			
+			
+			
+			this.destructores[0].dibujarse(this.entorno);
+			this.destructores[1].dibujarse(this.entorno);
+			this.destructores[2].dibujarse(this.entorno);
+			this.destructores[3].dibujarse(this.entorno);
+			
+			this.destructores[0].mover();
+			this.destructores[1].mover();
+			this.destructores[2].mover();
+			this.destructores[3].mover();
+			  
+			
+			
 			this.miAsteroide.mover();
+			this.miProyectil.mover();
+			
+			
 			if(this.miAsteroide.getY() > this.entorno.alto()) {
 				this.miAsteroide.setY(50);
 			}
-			if (this.miDestructores.getY()> this.entorno.alto() ) {
-				this.miDestructores.setY(50);
+			if (this.destructores[1].getY()> this.entorno.alto() ) {
+				this.destructores[1].setY(50);
 			}
 			if (this.entorno.estaPresionada(this.entorno.TECLA_DERECHA)
 					&& this.miAstromega.getX() + this.miAstromega.getAncho() / 2 < this.entorno.ancho()) {
@@ -80,11 +104,11 @@ public class Juego extends InterfaceJuego
 		}
 	}
 	private boolean choqueNaves() {
-		boolean superposicionY=this.miDestructores.getY()> this.miAstromega.getY()- this.miAstromega.getAlto()/2;
-		boolean superposicionX=(this.miAstromega.getX()- this.miAstromega.getAncho()/2 < this.miDestructores.getX()) &&
-				(this.miAstromega.getX()+this.miAstromega.getAncho()/2> this.miDestructores.getX());
+		boolean superposicionY=this.destructores[1].getY()> this.miAstromega.getY()- this.miAstromega.getAlto()/2;
+		boolean superposicionX=(this.miAstromega.getX()- this.miAstromega.getAncho()/2 < this.destructores[1].getX()) &&
+				(this.miAstromega.getX()+this.miAstromega.getAncho()/2> this.destructores[1].getX());
 		return superposicionY && superposicionX;
-	}
+}
 	private boolean choqueNaveAsteroide() {
 		boolean superposicionY=this.miAsteroide.getY()> this.miAstromega.getY()- this.miAstromega.getAlto()/2;
 		boolean superposicionX=(this.miAstromega.getX()- this.miAstromega.getAncho()/2 < this.miAsteroide.getX()) &&
