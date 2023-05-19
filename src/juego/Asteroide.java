a2db80f2fd38fb26de6b697f7d12e4868c08bc3d
micaelapalomeque <micaelapalomeque07@gmail.com>
2023-05-19 00:15:41 -0300

    modificaciones de Asteroide

--- src/juego/Asteroide.java
+++ src/juego/Asteroide.java
@@ -1,37 +1,74 @@
 package juego;
 
 import java.awt.Color;
+import java.util.Random;
 
 import entorno.Entorno;
 
 public class Asteroide {
-		private int x;
-		private int y;
+		private double x;
+		private double y;
 		private int radio;
 		private int velocidad;
+		private double angulo;
 		
 		Asteroide(int x, int y, int radio, int velocidad){
 			this.x=x;
 			this.y=y;
 			this.radio=radio;
 			this.velocidad=velocidad;
+			this.angulo=entorno.Herramientas.grados(135);
 		}
+		
 		public void dibujarse(Entorno entorno) {
 			entorno.dibujarCirculo(this.x, this.y, this.radio, Color.white);
-	}
-		public void mover() {
-			this.y+= velocidad;
 		}
-		public int getX() {
+		public void dibujarse2(Entorno entorno)
+		{
+			entorno.dibujarCirculo(this.x, this.y, this.radio, Color.pink);
+		}
+		
+		public void mover() {
+			this.y=this.y + velocidad*Math.sin(angulo);
+			this.x=this.x + velocidad*Math.cos(angulo);
+		}
+		
+		public void choqueDerecha() { //cambia de angulo cuando choca con la pared derecha
+			this.angulo=entorno.Herramientas.grados(315);
+		}
+		
+		public void choqueIzquierda() //cambia de angulo cuando choca con la parde izquierda
+		{
+			this.angulo= entorno.Herramientas.grados(135);
+		}
+		
+		public void respawn(Entorno entorno)
+		{
+			Random rand= new Random();
+			this.x= rand.nextInt(50, entorno.ancho()-50);
+			this.y= 100;
+		}
+		
+		public double getX() {
 			return x;
 		}
+		
 		public void setX(int x) {
 			this.x = x;
 		}
-		public int getY() {
+		
+		public double getY() {
 			return y;
+		}
+		public double getRadio()
+		{
+			return radio;
 		}
 		public void setY(int y) {
 			this.y = y;
+		}
+		public void setAngulo(double angulo)
+		{
+			this.angulo= entorno.Herramientas.grados(angulo);
 		}
 }
--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -12,10 +12,11 @@ public class Juego extends InterfaceJuego
 	// El objeto Entorno que controla el tiempo y otros
 	private Entorno entorno;
 	private Astromega miAstromega;
-	private Destructor destructores;
+	
 	private boolean perdido;
-	private Proyectil miProyectil;
+	
 	private Asteroide miAsteroide;
+	private Asteroide miAsteroide2;
 	
 	// Variables y métodos propios de cada grupo
 	// ...
@@ -29,12 +30,11 @@ public class Juego extends InterfaceJuego
 		// Inicializar lo que haga falta para el juego
 		// ...
 		this.miAstromega= new Astromega(400, 550, 150, 40,2);
+		/*this.destructores=new Destructor(rand.nextInt(650),100,50,50,2);
+		this.miProyectil= new Proyectil(450,300,15,50,2);*/
+		this.miAsteroide=new Asteroide(550,100,50,4);
+		this.miAsteroide2= new Asteroide(5,100,50,4);
 		
-		
-		
-		this.miProyectil= new Proyectil(450,300,15,50,2);
-		this.miAsteroide=new Asteroide(rand.nextInt(650),100,50,1);
-		this.destructores= new Destructor(rand.nextInt(650),200,50,50,2);
 
 		// Inicia el juego!
 		this.entorno.iniciar();
@@ -53,57 +53,125 @@ public class Juego extends InterfaceJuego
 		// ...
 		if (!perdido) {
 			this.miAstromega.dibujarse(this.entorno);
-			this.miAsteroide.dibujarse(this.entorno);
-			this.miProyectil.dibujarse(this.entorno);
-			this.destructores.dibujarse(entorno);
+			
+			/*this.miProyectil.dibujarse(this.entorno);
+			this.destructores.dibujarse(this.entorno);*/
+			this.miAsteroide.dibujarse2(this.entorno);
+			this.miAsteroide2.dibujarse(this.entorno);
 			
 			
 			
-			
-			  
-			
+			if(salirDerecha()) //el asteroide se mueve hacia la derecha con un angulo de 135
+			{
+				
+				this.miAsteroide.setAngulo(135);
+				this.miAsteroide2.setAngulo(135);
+				
+				
+				
+				
+			}
+			if (salirIzquierda()) //el asteroide se mueve hacia la izquierda en un angulo de 315
+			{
+				this.miAsteroide.setAngulo(315);
+				this.miAsteroide2.setAngulo(315);
+				
+				
+				
+			}
 			
 			this.miAsteroide.mover();
-			this.miProyectil.mover();
+			this.miAsteroide2.mover();
 			
 			
-			if(this.miAsteroide.getY() > this.entorno.alto()) {
-				this.miAsteroide.setY(50);
+			if (this.miAsteroide.getY()>= this.entorno.alto() ) //el asteroide reaparece si se pasa del alto del entorno
+			{
+				this.miAsteroide.respawn(this.entorno);
 			}
-			if (this.destructores.getY()> this.entorno.alto() ) {
-				this.destructores.setY(50);
+			if(this.miAsteroide2.getY()>= this.entorno.alto())
+			{
+				this.miAsteroide2.respawn(this.entorno);
 			}
+			
+			
+			if(this.miAsteroide.getX()>= this.entorno.ancho() ) //cambia de angulo cuando el x del asteroide supera el ancho del entorno
+			{
+				this.miAsteroide.choqueDerecha();
+			}
+			
+			if(this.miAsteroide2.getX()>= this.entorno.ancho())
+			{
+				this.miAsteroide2.choqueDerecha();
+			}
+			
+			
+			if(this.miAsteroide.getX() <= 0) //cambia de angulo cuando el x del asteroide es <=0
+			{
+				this.miAsteroide.choqueIzquierda();
+			}
+			
+			if(this.miAsteroide2.getX()<= 0)
+			{
+				this.miAsteroide2.choqueIzquierda();
+			}
+			
+			if(colisionDeAsteroides()) //por el momento solo pude hacer un choque xd
+			{
+				this.miAsteroide2.choqueDerecha();
+				this.miAsteroide.choqueIzquierda();
+			}
+			
+			
+			
 			if (this.entorno.estaPresionada(this.entorno.TECLA_DERECHA)
 					&& this.miAstromega.getX() + this.miAstromega.getAncho() / 2 < this.entorno.ancho()) {
 				this.miAstromega.moverDerecha();
 			}
-				if (this.entorno.estaPresionada(this.entorno.TECLA_IZQUIERDA)
-					&& this.miAstromega.getX() - this.miAstromega.getAncho() / 2 > 0) {
-				this.miAstromega.moverIzquierda();
 			
-				
+			if (this.entorno.estaPresionada(this.entorno.TECLA_IZQUIERDA)
+				&& this.miAstromega.getX() - this.miAstromega.getAncho() / 2 > 0) {
+				this.miAstromega.moverIzquierda();
 			}
-				if (choqueNaves() || choqueNaveAsteroide()) {
-					this.perdido=true;
-				}
+			
+			/*if (choqueNaves() || choqueNaveAsteroide()) {
+				this.perdido=true;
+			}*/
 		}
 		else {
 			this.entorno.cambiarFont(Font.SANS_SERIF,100,Color.RED);
 			this.entorno.escribirTexto("Perdiste", this.entorno.ancho()/2,this.entorno.alto()/2 );
 		}
 	}
-	private boolean choqueNaves() {
+	/*private boolean choqueNaves() {
 		boolean superposicionY=this.destructores.getY()> this.miAstromega.getY()- this.miAstromega.getAlto()/2;
 		boolean superposicionX=(this.miAstromega.getX()- this.miAstromega.getAncho()/2 < this.destructores.getX()) &&
 				(this.miAstromega.getX()+this.miAstromega.getAncho()/2> this.destructores.getX());
 		return superposicionY && superposicionX;
-}
+		*/
+
 	private boolean choqueNaveAsteroide() {
 		boolean superposicionY=this.miAsteroide.getY()> this.miAstromega.getY()- this.miAstromega.getAlto()/2;
 		boolean superposicionX=(this.miAstromega.getX()- this.miAstromega.getAncho()/2 < this.miAsteroide.getX()) &&
 				(this.miAstromega.getX()+this.miAstromega.getAncho()/2> this.miAsteroide.getX());
 		return superposicionY && superposicionX;
 	}
+	private boolean salirDerecha() //da verdadero si el asteroide sale desde x=0 hasta la mitad del ancho del entorno
+	{
+		boolean xMitad= this.miAsteroide.getX()>=0 && this.miAsteroide.getX() <= this.entorno.ancho()/2;
+		boolean y= this.miAsteroide.getY()== 100;
+		return xMitad && y ;
+	}
+	private boolean salirIzquierda() //da verdadero si el asteroide sale desde la mitad del ancho hasta el fin del ancho del entorno
+	{
+		boolean xMitad= this.miAsteroide.getX()> this.entorno.ancho()/2 && this.miAsteroide.getX() <=650;
+		boolean y= this.miAsteroide.getY() ==100;
+		return xMitad &&y;
+	}
+	private boolean colisionDeAsteroides()
+	{
+		return this.miAsteroide.getX()- this.miAsteroide.getRadio()< this.miAsteroide2.getX();
+	}
+	
 	
 	@SuppressWarnings("unused")
 	public static void main(String[] args)
